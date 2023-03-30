0a619f7f772d9e0fdf780cb56b36211bc4c1eca9
leijingxing <1276753397@qq.com>
2023-03-30 10:28:49 +0800

    爬虫

--- pa_tupian.py
+++ pa_tupian.py
@@ -2,16 +2,24 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-url = 'http://www.netbian.com/shouji/dongman/index_3.htm'
-# 获取网页
-response = requests.get(url)
-# 解析网页
-soup = BeautifulSoup(response.text, 'html.parser')
-# 获取图片标签
-img_tags = soup.find_all('img')
+# 设置请求头
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-# 获取图片链接
-urls = [img['src'] for img in img_tags]
+# 遍历所有页面
+for i in range(1, 50):
+    url = f'http://www.netbian.com/dongwu/index_{i}.htm'
+    if i == 1:
+        url = f'http://www.netbian.com/dongwu'
+    # 获取网页
+    response = requests.get(url, headers=headers,)
+    # 解析网页
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # 获取图片标签
+    img_tags = soup.find_all('img')
+
+    # 获取图片链接
+    urls = [img['src'] for img in img_tags]
 
 # 创建文件夹
 if not os.path.exists('images'):
