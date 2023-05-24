3a667948ae19a74002b30916fe7300488b378b89
leijingxing <1276753397@qq.com>
2023-05-24 11:04:28 +0800

    啊啊啊啊

--- pa_tupian.py
+++ pa_tupian.py
@@ -1,3 +1,4 @@
+# 退出程序
 import requests
 import os
 from bs4 import BeautifulSoup
@@ -7,6 +8,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 # 遍历所有页面
+urls = []
 for i in range(1, 50):
     url = f'http://www.netbian.com/dongwu/index_{i}.htm'
     if i == 1:
@@ -19,7 +21,7 @@ for i in range(1, 50):
     img_tags = soup.find_all('img')
 
     # 获取图片链接
-    urls = [img['src'] for img in img_tags]
+    urls += [img['src'] for img in img_tags]
 
 # 创建文件夹
 if not os.path.exists('images'):
@@ -33,4 +35,4 @@ for url in urls:
         f.write(requests.get(url).content)
 
 # 退出程序
-exit()
+
